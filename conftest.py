8b231b3e3072b201c12174f3cc1418589699b253
Dinhar <vlaser0996@gmail.com>
2023-02-02 17:51:32 +0300

    first test

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                    help="Choose language: ru or fr")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+    if user_language in ['ru', 'en', 'fr']:
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--language should be ru, en or fr")
+    yield browser
+    browser.quit()
